53e1b2cdd6718d07a556e0c24147269a9d7f9e33
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-22 14:48:46 +0300

    Changed the react fragment tags to a div

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Greeting = () =>
 {
     return (
-        <>
+        <div>
             <h2>Hello World</h2>
             <p> Insert Dumb Programming Joke</p>
-        </>
+        </div>
     )
 }
 
