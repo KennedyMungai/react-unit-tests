5ba92e9252770552b2e4d6d61d55f691f5d32aaa
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-22 20:13:27 +0300

    Stored a reference for all listItemElements

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -1,11 +1,16 @@
 import { render, screen } from "@testing-library/react"
 import Async from "./Async"
 
-describe('Async component ', () => 
+describe('Async component', () => 
 {
-    test('renders posts correctly', () =>
+    test('renders posts if request succeeds', () =>
     {
         // Arrange
         render(<Async />)
+
+        // Act
+
+        // Assert
+        const listItemElements = screen.getAllByRole('listitem')
     })
 })
